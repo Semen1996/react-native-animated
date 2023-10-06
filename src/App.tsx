8115d46469cafe25aa04496d387eed656c66482b
Semen Tolstoguzov <semensemenovichsemenov2020@yandex.ru>
2023-10-06 11:29:46 +0300

    ref: отформатировал app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import {View, StatusBar} from 'react-native';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import store from './store';
 import Navigator from './navigation/Navigator';
-
-
 
 function App() {
   return (
     <Provider store={store}>
-    <View style={{backgroundColor: '#FFFFFF', flex: 1}}>
-      <StatusBar backgroundColor={'#FFFFFF'} barStyle={'dark-content'} />
-      <Navigator />
-    </View>
+      <View style={{backgroundColor: '#FFFFFF', flex: 1}}>
+        <StatusBar backgroundColor={'#FFFFFF'} barStyle={'dark-content'} />
+        <Navigator />
+      </View>
     </Provider>
   );
 }
