dc4e520043d5dab3a278331c9e7277b06a57609a
Semen Tolstoguzov <semensemenovichsemenov2020@yandex.ru>
2023-10-12 23:51:36 +0300

    feat: добавили функционал  btn изменить заявление

--- src/screens/PetitionStack/ReadyScreen.tsx
+++ src/screens/PetitionStack/ReadyScreen.tsx
@@ -8,6 +8,8 @@ import ButtonBlack from "../../components/buttons/ButtonBlack";
 import MoneyIcon from '../../images/Icons/payments.svg';
 import PreviewIcon from '../../images/Icons/preview.svg';
 import EditIcon from '../../images/Icons/edit.svg';
+import DownloadIcon from '../../images/Icons/download.svg';
+import MailIcon from '../../images/Icons/mail_stroke.svg';
 import colors from "../../utils/colors";
 import ProgressBar from "../../components/PetitionScreen/ProgressBar";
 import { RootStackParamList } from "../../navigation/Navigator";
@@ -70,15 +72,10 @@ function ReadyScreen({navigation, route}: Props) {
   function handleContinue() {
     if(petition) {
       dispatch(changeCurrentID({id: petition.id}));
-      
-      const questions = Object.values(petition.questions);
-      const nonFillQuestion = questions.find(q => q.isFill === false);
-      if(nonFillQuestion) {
-        if(petition.form === 'RVPForm') {
-          navigation.navigate('RVPForm', {screen: nonFillQuestion.screen});
-        } else if(petition.form === 'WorkingForm') {
-          navigation.navigate('WorkingForm', {screen: nonFillQuestion.screen});
-        }
+      if(petition.form === 'RVPForm') {
+        navigation.navigate('RVPForm', {screen: 'Questions'});
+      } else if(petition.form === 'WorkingForm') {
+        navigation.navigate('WorkingForm', {screen: 'Questions'});
       }
     }
   }
@@ -116,18 +113,37 @@ function ReadyScreen({navigation, route}: Props) {
           </View>
           <ProgressBar procent={procent} />
           <View style={styles.buttons}>
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity onPress={handleContinue} style={styles.button}>
               <View style={{width: 40, height: 40, justifyContent: 'center', alignItems: 'center', borderRadius: 100, backgroundColor: '#EAE6FF'}}>
                 <EditIcon width={24} height={24} />
               </View>
               <Text style={[globalStyles.text, globalStyles.text16Med, styles.buttonText]}>Изменить заявление</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
-              <View style={{width: 40, height: 40, justifyContent: 'center', alignItems: 'center', borderRadius: 100, backgroundColor: '#EAE6FF'}}>
-                <PreviewIcon width={24} height={24} />
-              </View>
-              <Text style={[globalStyles.text, globalStyles.text16Med, styles.buttonText]}>Предварительный просмотр</Text>
-            </TouchableOpacity>
+            {
+              !isPay ?
+              <TouchableOpacity style={styles.button}>
+                <View style={{width: 40, height: 40, justifyContent: 'center', alignItems: 'center', borderRadius: 100, backgroundColor: '#EAE6FF'}}>
+                  <PreviewIcon width={24} height={24} />
+                </View>
+                <Text style={[globalStyles.text, globalStyles.text16Med, styles.buttonText]}>Предварительный просмотр</Text>
+              </TouchableOpacity>
+              :
+              <>
+                <TouchableOpacity style={styles.button}>
+                  <View style={{width: 40, height: 40, justifyContent: 'center', alignItems: 'center', borderRadius: 100, backgroundColor: '#EAE6FF'}}>
+                    <DownloadIcon width={24} height={24} />
+                  </View>
+                  <Text style={[globalStyles.text, globalStyles.text16Med, styles.buttonText]}>Скачать заявление</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}>
+                  <View style={{width: 40, height: 40, justifyContent: 'center', alignItems: 'center', borderRadius: 100, backgroundColor: '#EAE6FF'}}>
+                    <MailIcon width={24} height={24} />
+                  </View>
+                  <Text style={[globalStyles.text, globalStyles.text16Med, styles.buttonText]}>Отправить на почту</Text>
+                </TouchableOpacity>
+              </>
+            }
+
           </View>
         </View>
         {
