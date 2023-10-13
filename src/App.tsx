fe8404a09124cd438f4d378a9382e8bdfd562e10
Semen Tolstoguzov <semensemenovichsemenov2020@yandex.ru>
2023-10-13 10:59:54 +0300

    feat: сохранение данных в локал сторидж

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import {View, StatusBar} from 'react-native';
 import {Provider} from 'react-redux';
-import store from './store';
+import store, { persistor } from './store';
 import Navigator from './navigation/Navigator';
+import { PersistGate } from 'redux-persist/integration/react';
 
 function App() {
   return (
     <Provider store={store}>
-      <View style={{backgroundColor: '#FFFFFF', flex: 1}}>
-        <StatusBar backgroundColor={'#FFFFFF'} barStyle={'dark-content'} />
-        <Navigator />
-      </View>
+      <PersistGate persistor={persistor} loading={null}>
+        <View style={{backgroundColor: '#FFFFFF', flex: 1}}>
+          <StatusBar backgroundColor={'#FFFFFF'} barStyle={'dark-content'} />
+          <Navigator />
+        </View>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,12 +1,38 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+// import storage from 'redux-persist/lib/storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import petitionSlice from "./petitionSlice";
 
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const rootReducer = combineReducers({
+  petitions: petitionSlice,
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: {
-    petitions: petitionSlice,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
 
+export const persistor = persistStore(store);
 export default store;
 
 export type RootState = ReturnType<typeof store.getState>;
